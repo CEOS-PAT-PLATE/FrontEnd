9ced0442ec4fac628a088e4225124e9cb730041b
Dahn12 <dadesign0709@gmail.com>
2024-07-03 00:53:25 +0900

    feat : styled-components 설치

---
+++ pet-plate-front/src/app/globalstyles.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import 'reset-css';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: aliceblue;
+  }
+`;
+
+export default GlobalStyle;
--- pet-plate-front/src/app/layout.tsx
+++ pet-plate-front/src/app/layout.tsx
@@ -1,13 +1,17 @@
-import './globals.css';
-
+import StyledComponentsRegistry from './lib/registry'
+ 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
-  );
+  )
 }
