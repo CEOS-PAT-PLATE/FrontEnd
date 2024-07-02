d7b021bd3e9627e74dd3e37fe0303629997b5f9c
Dahn12 <dadesign0709@gmail.com>
2024-07-03 01:03:24 +0900

    feat: GlobalStyles 적용

--- pet-plate-front/src/app/style/GlobalStyles.tsx
+++ pet-plate-front/src/app/style/GlobalStyles.tsx
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import 'reset-css';
 
-const GlobalStyle = createGlobalStyle`
-  body{
+const GlobalStyles = createGlobalStyle`
+  
+  body {
     background-color: aliceblue;
   }
+
 `;
 
-export default GlobalStyle;
+export default GlobalStyles;
