ff7a2dafb432fb39b7791223fee19361beed5fc3
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-04 23:26:24 +0900

    feat : globalstyle 설정, input data 2 app routing 구조

--- pet-plate-front/src/app/page.tsx
+++ pet-plate-front/src/app/page.tsx
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <main>
-      HI
-    </main>
-  )
+  return <main>HI</main>
 }
--- pet-plate-front/src/app/style/GlobalStyles.tsx
+++ pet-plate-front/src/app/style/GlobalStyles.tsx
@@ -1,12 +1,22 @@
-import { createGlobalStyle } from 'styled-components';
-import 'reset-css';
+import { createGlobalStyle } from 'styled-components'
+import 'reset-css'
 
 const GlobalStyles = createGlobalStyle`
-  
-  body {
-    background-color: ${(props) => props.theme.colors['green-900']}; //theme 사용 문법 예시 
+  html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    background-color:${(props) => props.theme.colors['green']}; // 전체 배경색 설정
   }
 
-`;
+  body {
+    background-color: ${(props) => props.theme.colors['grey1']}; // body 배경색 설정
+    width: 360px;
+    height: 753px;
+    margin: auto;
+    position: relative;
+  }
+`
 
-export default GlobalStyles;
+export default GlobalStyles
