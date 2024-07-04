3bbc9293cfc233217d6f08550a17b3c3c5102256
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-05 03:02:13 +0900

    feat : inputdata2 - navbar 구현

---
+++ pet-plate-front/src/app/input-data2/natural-food/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <div>
+        <h1>자연식</h1>
+      </div>
+    )
+  }
+  
---
+++ pet-plate-front/src/app/input-data2/packaged-snacks/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <div>
+        <h1>포장 간식</h1>
+      </div>
+    )
+  }
+  
