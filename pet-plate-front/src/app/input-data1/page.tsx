cde47886f6c3777caf2e5196b8534767fc4002cb
Dahn12 <dadesign0709@gmail.com>
2024-07-06 04:06:34 +0900

    feat : inputField 틀잡기

--- pet-plate-front/src/app/input-data1/page.tsx
+++ pet-plate-front/src/app/input-data1/page.tsx
@@ -1,13 +1,24 @@
+'use client'
+
+import styled from "styled-components"
 import InputField from "@components/input-data1/inputField"
 import Radio from "@components/input-data1/radio"
 
 
 export default function Page() {
     return (
-      <div>
-        <InputField/>
+      <Container>
+        <InputField 
+          width = "19.5rem"
+          placeholder = "메인 페이지"
+        />
         <Radio/>
-      </div>
+      </Container>
     )
   }
   
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
--- pet-plate-front/src/components/input-data1/inputField.tsx
+++ pet-plate-front/src/components/input-data1/inputField.tsx
@@ -1,10 +1,44 @@
+'use client'
+
 import styled from "styled-components"
 
-export default function inputField() {
+interface InputFieldProps {
+    width: string;
+    placeholder: string;
+}
+
+export default function inputField({ width, placeholder } : InputFieldProps) {
   return (
-    <div>
-        inputField
-    </div>
+    <InputFiledWrapper>
+        <InputFileld type="text"
+         placeholder={placeholder} 
+         width={width}
+        />
+    </InputFiledWrapper>
   )
 }
 
+const InputFiledWrapper = styled.div`
+    
+`
+
+const InputFileld = styled.input`
+    height: 3rem;
+    padding: 0 1rem;
+    font-size: 1rem;
+    font-weight: 400;
+    width: ${(props) => props.width};
+    border: solid 0.1px #BDC5CC; // 피그마에 명시 안되어있음, grey5
+    border-radius: 0.6rem;
+    color: #36393C; //grey11
+
+    &::placeholder {
+        color: #AFB8C1; //grey6
+    }
+
+    &:focus{
+        outline: none;
+        border-color: #40C97F; //green500
+    }
+
+`
