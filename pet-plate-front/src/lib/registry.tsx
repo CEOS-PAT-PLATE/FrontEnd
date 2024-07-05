ffc60068c2ff4f14e1ade9ac16e5160a71ccbb46
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-06 04:27:14 +0900

    style : theme 적용 리팩토링

--- pet-plate-front/src/lib/registry.tsx
+++ pet-plate-front/src/lib/registry.tsx
@@ -1,36 +1,23 @@
-'use client'
- 
-import React, { useState } from 'react'
-import { useServerInsertedHTML } from 'next/navigation'
-import { ServerStyleSheet, StyleSheetManager, ThemeProvider } from 'styled-components'
-import GlobalStyles from '@style/GlobalStyles'
-import theme from '@style/theme'
- 
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+'use client';
+
+import React, { useState } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { ServerStyleSheet, StyleSheetManager, ThemeProvider } from 'styled-components';
+import GlobalStyles from '@style/GlobalStyles';
+import theme from '@style/theme';
+
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
- 
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
   useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement()
-    styledComponentsStyleSheet.instance.clearTag()
-    return <>{styles}</>
-  })
- 
-  if (typeof window !== 'undefined') return <>{children}</>
- 
-  return (
-    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyles />
-          {children}
-        </>
-      </ThemeProvider>
-    </StyleSheetManager>
-  )
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') return <>{children}</>;
+
+  return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
 }
