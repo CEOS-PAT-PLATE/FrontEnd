66100ac5f47dee12809103b9c4eb9559c4105203
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-05 17:54:46 +0900

    feat : 자연식 관련 type 인터페이스

--- pet-plate-front/src/app/input-data2/layout.tsx
+++ pet-plate-front/src/app/input-data2/layout.tsx
@@ -17,8 +17,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 }
 
 const GridContainer = styled.div`
+  background-color: ${(props) =>
+    props.theme.colors['grey1']}; // body 배경색 설정
+
   display: grid;
- align-items: center;
+  align-items: center;
   grid-template-areas:
     'navbar navbar exit-button'
     'content content content'; /* grid-area 이름 설정 */
@@ -32,7 +35,8 @@ const ExitButtonImage = styled(Image)`
   grid-area: exit-button; /* 이미지를 exit-button 영역에 배치 */
   position: relative; /* 절대적인 위치를 고정 */
   top: 14px; /* 화면 상단에 고정 */
-  left: -3px; /* 좌측 정렬 */`
+  left: -3px; /* 좌측 정렬 */
+`
 
 const Content = styled.div`
   grid-area: content; /* Content를 content 영역에 배치 */
