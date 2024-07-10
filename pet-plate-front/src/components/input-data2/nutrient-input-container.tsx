91a7be47d1141ed1b42dadb7b595080320365a97
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-11 01:26:19 +0900

    <fix> 해시 함수를 사용하여 고유한 key 경고 해결
    - 각 nutrient 섹션에 대해 고유한 key 생성을 위해 간단한 해시 함수 추가
    - map 함수 내의 최상위 div에 해시 함수를 적용하여 key 설정
    - 모든 자식 요소가 고유한 key를 가지도록 수정하여 React 경고 해결
    
    "Each child in a list should have a unique 'key' prop." 경고 메시지 해결

--- pet-plate-front/src/components/input-data2/nutrient-input-container.tsx
+++ pet-plate-front/src/components/input-data2/nutrient-input-container.tsx
@@ -1,38 +1,36 @@
-'use client'
+'use client';
 
 import styled from 'styled-components';
 import NutrientInputFieldsSection from '@components/input-data2/nutrient-input-section';
-
+import { simpleHash } from '@lib/utils';
 
 
 interface Nutrient {
-    name: string;
-    unit: string;
-    isRequired: boolean;
-  }
-
+  name: string;
+  unit: string;
+  isRequired: boolean;
+}
 
 interface NutrientSection {
-    nutrients: Nutrient[];
-  }
-  
-  interface NutrientInputFieldsContainerProps {
-    nutrientSections: NutrientSection[];
-  }
-  
+  nutrients: Nutrient[];
+}
 
-  
+interface NutrientInputFieldsContainerProps {
+  nutrientSections: NutrientSection[];
+}
 
+const NutrientInputFieldsContainer = ({ nutrientSections }: NutrientInputFieldsContainerProps) => {
 
 
-const NutrientInputFieldsContainer = ({ nutrientSections }:NutrientInputFieldsContainerProps ) => {
+
   return (
-    <NutrientInputContainer>
+     <NutrientInputContainer>
       {nutrientSections.map((section, index) => (
-        <>
-        <NutrientInputFieldsSection key={index} nutrients={section.nutrients} />
-        <SectionBorder/>
-        </>
+         <div key={simpleHash(JSON.stringify(section))}>
+         <NutrientInputFieldsSection nutrients={section.nutrients} />
+         <SectionBorder />
+       </div>
+      
       ))}
     </NutrientInputContainer>
   );
@@ -40,23 +38,17 @@ const NutrientInputFieldsContainer = ({ nutrientSections }:NutrientInputFieldsCo
 
 export const NutrientInputContainer = styled.div`
   overflow-y: scroll;
-  margin-top:14px;
-    display: grid;
-      height: 280px;
-
-
-
+  margin-top: 14px;
+  display: grid;
+  height: 280px;
 `;
 
- const SectionBorder=styled.div`
- 
- width: 312px;
- background: #DDE0E4;
-height: 1px;
-margin-top:11px;
-margin-bottom:11px;
-`
-
+const SectionBorder = styled.div`
+  width: 312px;
+  background: #dde0e4;
+  height: 1px;
+  margin-top: 11px;
+  margin-bottom: 11px;
+`;
 
 export default NutrientInputFieldsContainer;
-
