d6e7cf599c3d2633cf00903a20fe123cf20b77b6
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-11 01:28:18 +0900

    <feat>: API 테스트를 위한 env 파일 및 자연식 fetch 함수 구현
    - API 테스트를 위한 .env 파일 추가 및 설정
    - 서버 컴포넌트에서 async/await를 사용하여 자연식 데이터를 가져오는 간단한 fetch 함수 구현 (아직 리액트 쿼리 적용 이전 테스트 용도)
    - 환경 변수를 사용하여 API URL 및 액세스 토큰 관리
    - git ignore .env 추가

--- pet-plate-front/src/app/input-data2/natural-food/page.tsx
+++ pet-plate-front/src/app/input-data2/natural-food/page.tsx
@@ -2,7 +2,36 @@ import Search from '@components/input-data2/search';
 import Table from '@components/input-data2/table';
 import InfoLayout from '@components/input-data2/info-layout';
 
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN as string;
+
+  // 기본 fetch 함수로 테스트 
+  export async function getNaturalFoodLists(keyword:string) {
+    const url = new URL(API_URL);
+    url.searchParams.append('keyword', keyword);
+  
+    const response = await fetch(url.toString(), {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+  
+    });
+
+  
+    if (!response.ok) {
+      throw new Error(`에러 ${response.statusText}`);
+    }
+  
+    return response.json();
+  }
+
 export default async function Page() {
+
+  const data = await getNaturalFoodLists('고기');
+  console.log(data);
+
+  
   return (
     <>
       <InfoLayout
