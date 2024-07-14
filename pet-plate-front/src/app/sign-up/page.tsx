76423c13ff5950fc8242dd67a9bf1c895d8964ad
Dahn12 <dadesign0709@gmail.com>
2024-07-14 18:54:20 +0900

    feat: signup1(main) page 틀잡기

--- pet-plate-front/src/app/sign-up/page.tsx
+++ pet-plate-front/src/app/sign-up/page.tsx
@@ -1,7 +1,94 @@
-import React from 'react'
+'use client'
+
+import styled from "styled-components"
+import Image from "next/image"
+import Link from 'next/link'
+
+import signUpGraphic from "@public/svg/signup1.svg?url"
+import mainLogo from "@public/svg/main-logo.svg?url"
+import NaverLogo from "@public/svg/Naver.svg?url"
+import CenterButton from "@components/main/centerbtn"
+
 
 export default function page() {
+const handleOnclick = () =>{}
+const buttonContent = (
+  <>
+      <Image src={NaverLogo} alt="Naver-logo"/>
+      <span style={{color : "#fff", marginLeft : "1.438rem"}}>네이버로 시작하기</span>
+  </>
+);
   return (
-    <div>처음 페이지</div>
+    <PageWrapper>
+      <Padding/>
+      <ContentWrapper>
+        <Text>하루 10분, 펫플레이트로 건강 체크!</Text>
+        <MainLogo src={mainLogo} alt="mainLogo"/>
+        <SignUpGraphic src={signUpGraphic} alt="signUpGraphic"/>
+      </ContentWrapper>
+      <ButtonWrapper>
+        <CenterButton onClick={handleOnclick}
+          backgroundColor= "#03C75A" 
+          hoverBackgroundColor= "#03C75A" 
+          hoverButtonContentColor= "#fff"
+          buttonContent = {buttonContent}
+        />
+        <TextLink href="/main">로그인 없이 둘러보기</TextLink>
+      </ButtonWrapper>
+    </PageWrapper>
   )
 }
+
+
+const PageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`
+const Padding = styled.div`
+  width: 100%;
+  height: 44px;
+  //상단 탭 임의 설정
+`
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4rem;
+`
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 4.25rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.25rem;
+`
+
+
+const Text = styled.div`
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 160%;
+  color: ${(props) => props.theme.colors['grey9']};
+`
+
+const TextLink = styled(Link)`
+  font-size: 0.625rem;
+  font-weight: 400;
+  line-height: 160%;
+  color: ${(props) => props.theme.colors['grey9']};
+`
+
+const SignUpGraphic = styled(Image)`
+  
+`
+const MainLogo = styled(Image)`
+  margin-top: 0.625rem;
+  margin-bottom: 2.644rem;
+`
--- pet-plate-front/src/components/main/centerbtn.tsx
+++ pet-plate-front/src/components/main/centerbtn.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+'use client'
 
-export default function centerbtn() {
+import styled from "styled-components"
+import { ReactNode } from "react"
+
+interface CenterButtonProps {
+    onClick: () => void;
+    buttonContent: ReactNode;
+    backgroundColor: string;
+    hoverBackgroundColor: string;
+    hoverButtonContentColor: string;
+}
+
+export default function CenterButton({onClick, buttonContent, backgroundColor, hoverBackgroundColor, hoverButtonContentColor} : CenterButtonProps) {
   return (
-    <div>centerbtn</div>
+    <ButtonContainer 
+      onClick={onClick} 
+      backgroundColor={backgroundColor} 
+      hoverBackgroundColor={hoverBackgroundColor}
+      hoverButtonContentColor={hoverButtonContentColor}
+    >
+        <ButtonContent>{buttonContent}</ButtonContent>
+    </ButtonContainer>
   )
 }
+
+const ButtonContainer = styled.div<{ backgroundColor: string, hoverBackgroundColor: string, hoverButtonContentColor: string }>`
+    width: 19.5rem;
+    height: 3.563rem;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: ${({ backgroundColor }) => backgroundColor};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    &:hover{
+        background-color: ${({ hoverBackgroundColor }) => hoverBackgroundColor};
+
+    }
+`
+
+const ButtonContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 1.125rem;
+  font-weight: 600;
+`
