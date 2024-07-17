1ef0a9a6aeccf6d2d5d91410038a8c26ad2a4765
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-18 01:42:35 +0900

    <feat> [결과페이지] 로딩 상태 구현 테스트
    결과 분석 완료, 페이지 로딩 전에 띄울 로딩 페이지 promise 객체와 settimeout으로 테스트

---
+++ pet-plate-front/src/app/result/page.tsx
@@ -0,0 +1,16 @@
+export default async function Page() {
+  
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('j');
+        }, 1000);
+    }
+    );
+
+
+  return (
+    <div>
+      <h1>Input Data 2</h1>
+    </div>
+  );
+}
