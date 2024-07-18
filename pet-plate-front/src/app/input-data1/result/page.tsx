38465ee53adebaa58e86c32b2184255cf20ab226
Dahn12 <dadesign0709@gmail.com>
2024-07-19 04:25:09 +0900

    feat : alert page 틀 완성

--- pet-plate-front/src/app/input-data1/result/page.tsx
+++ pet-plate-front/src/app/input-data1/result/page.tsx
@@ -1,32 +1,59 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import InputDataFirstHeader from '@components/input-data1/inputDataFirstHeader';
 import Progressbar from '@components/input-data1/progressbar';
 import NextButton from '@components/input-data1/nextButton';
 import ResultList from '@components/input-data1/resultList';
+import { petAPI } from '@api/petAPI';
 
-import { useRecoilValue } from 'recoil';
-import { petInfoState } from '@lib/atoms';
+interface Pet {
+  petId: number;
+  name: string;
+  age: number;
+  weight: number;
+  activity: string;
+  neutering: string;
+  profileImgPath: string | null;
+}
 
-export default function page() {
-  const petInfo = useRecoilValue(petInfoState);
+export default function Page() {
+  const [pets, setPets] = useState<Pet[]>([]);
 
-  const navigte = () => {};
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const response = await petAPI.getAllPetsInfo();
+        setPets(response.data.data); 
+      } catch (error) {
+        console.error('Failed to fetch pets', error);
+      }
+    };
+
+    fetchPets();
+  }, []);
+
+  console.log(pets)
+
+  const navigate = () => {};
 
   const handleAlert = () => {};
 
   return (
     <>
-      <InputDataFirstHeader onClickBackButton={navigte} />
+      <InputDataFirstHeader onClickBackButton={navigate} />
       <Progressbar />
       <PageContainer>
-        <ResultList title="반려견의 이름" value={petInfo[0].name} />
-        <ResultList title="나이" value={`${petInfo[0].age}세`} />
-        <ResultList title="몸무게" value={`${petInfo[0].weight}kg`} />
-        <ResultList title="활동량" value={petInfo[0].activity} />
-        <ResultList title="중성화 여부" value={petInfo[0].neutering} />
-
+        {pets.map((pet) => (
+          <React.Fragment key={pet.petId}>
+            <ResultList title="반려견의 이름" value={pet.name} />
+            <ResultList title="나이" value={`${pet.age}세`} />
+            <ResultList title="몸무게" value={`${pet.weight}kg`} />
+            <ResultList title="활동량" value={pet.activity} />
+            <ResultList title="중성화 여부" value={pet.neutering} />
+          </React.Fragment>
+        ))}
         <FixedButtonContainer>
           <NextButton onClick={handleAlert} />
         </FixedButtonContainer>
