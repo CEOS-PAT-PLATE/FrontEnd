2da3043fcbb6e847de961db0937949155874ccfa
Dahn12 <dadesign0709@gmail.com>
2024-07-25 22:10:25 +0900

    feat : 로그아웃 구현

--- pet-plate-front/src/api/userAPI.tsx
+++ pet-plate-front/src/api/userAPI.tsx
@@ -5,7 +5,16 @@ export const userAPI = {
   // 유저의 자신의 프로필 조회
   getUserInfo: async () => {
     return await axiosInstance.get(`/users/my-profile`);
-  }
+  },
+
+  //로그아웃
+  logout: async (accessToken: string) => {
+    return await axiosInstance.post('/auth/logout', null, {
+        headers: {
+            accessToken,
+        },
+    });
+},
 
 }
 /**
