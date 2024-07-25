8025d977f0178ae11b1142d78cc36605b8e219fa
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2024-07-26 01:49:58 +0900

    <fix> 다른 페이지 이동후에도 -> 언제든지 모달 종료되도록

--- pet-plate-front/src/app/201/@ExitModal/page.tsx
+++ pet-plate-front/src/app/201/@ExitModal/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
       setIsExitModalOpen(false);
     } else {
       router.push('/main/analyze');
-      //setIsExitModalOpen(false);
+      setIsExitModalOpen(false);
     }
   };
 
